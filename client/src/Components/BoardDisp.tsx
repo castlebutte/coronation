9ac8c8967adbaed9ff10b93f99177f258ecd688a
wchorkaw <wchorkaw@ualberta.ca>
2021-01-30 15:51:07 -0700

    switched black and white

--- client/src/Components/BoardDisp.tsx
+++ client/src/Components/BoardDisp.tsx
@@ -5,7 +5,7 @@ export default function BoardDisp({ board }: { board: Board }) {
       {board.arr.map((row, i) => (
         <div className="row" key={`${i}`}>
           {row.map((col, j) => (
-            <div className={( (i+j) % 2 ) ? "col" : "black" } key={`${i}-${j}`}>
+            <div className={( (i+j) % 2 ) ? "black" : "col" } key={`${i}-${j}`}>
               {/* Piece or null */}
               {JSON.stringify(col)}
             </div>
