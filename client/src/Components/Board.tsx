2f41c2d398985865ea427e41c07e02535c72d754
Charles Ancheta <cancheta@ualberta.ca>
2021-01-30 15:35:16 -0700

    oop

--- client/src/Components/Board.tsx
+++ client/src/Components/Board.tsx
@@ -1,20 +1,12 @@
-export default function Board() {
-  const board = [
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-    [1, 2, 3, 4, 5, 6, 7, 8],
-  ];
+import { Board } from "../../../types";
+export default function Board({ arr }: Board) {
   return (
     <>
-      {board.map((row, i) => (
+      {arr.map((row, i) => (
         <div className="row" key={`${i}`}>
           {row.map((col, j) => (
             <div className="col" key={`${i}-${j}`}>
+              {/* Piece or null */}
               {col}
             </div>
           ))}
