498271c631af1ebf114b6891c3be920c0d52d041
Annette Lau <annette1@ualberta.ca>
2021-01-30 10:46:26 -0700

    kahfdkjhsak

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRounter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 
 import {
@@ -7,7 +7,7 @@ import {
   HostJoinScreen,
   WaitingScreen,
   GameScreen,
-} from "./Components";
+} from "./components";
 
 export default function App() {
   return (
