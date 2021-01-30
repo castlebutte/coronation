e1bd05cc74849c4b84cde39091e4d802ccb59835
Annette Lau <annette1@ualberta.ca>
2021-01-30 10:39:51 -0700

    ROUTES WOOOOO

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import {BrowserRounter, Route} from "react-router-dom";
-import './App.css';
+import React from "react";
+import { BrowserRounter, Route } from "react-router-dom";
+import "./App.css";
 
-import {HomeScreen, HostJoinGame, from './Components/HomeScreen';
-
-
+import {
+  HomeScreen,
+  HostJoinScreen,
+  WaitingScreen,
+  GameScreen,
+} from "./Components";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <BrowserRounter>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/hostjoin" component={HostJoinScreen} />
+          <Route exact path="/waiting" component={WaitingScreen} />
+          <Route exact path="/game" component={GameScreen} />
+        </BrowserRounter>
       </header>
     </div>
   );
 }
-
