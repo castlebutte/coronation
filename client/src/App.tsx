cd21593983787a324475086a8f5bb10771d33515
wchorkaw <wchorkaw@ualberta.ca>
2021-01-30 10:51:11 -0700

    fixed components import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 
-import {
-  HomeScreen,
-  HostJoinScreen,
-  WaitingScreen,
-  GameScreen,
-} from "./components";
+import HomeScreen from "./Components/HomeScreen";
+import HostJoinScreen from "./Components/HostJoinScreen"
+import WaitingScreen from "./Components/WaitingScreen"
+import GameScreen from "./Components/GameScreen"
+
+
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRounter>
+        <BrowserRouter>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/hostjoin" component={HostJoinScreen} />
           <Route exact path="/waiting" component={WaitingScreen} />
           <Route exact path="/game" component={GameScreen} />
-        </BrowserRounter>
+        </BrowserRouter>
       </header>
     </div>
   );
