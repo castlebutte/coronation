529c85d5d6663374c62e160307127389185067b2
Annette Lau <annette1@ualberta.ca>
2021-01-30 11:36:43 -0700

    IT ISN"T WORKING ANYMORE FML

--- client/src/Components/HomeScreen.tsx
+++ client/src/Components/HomeScreen.tsx
@@ -1,21 +1,10 @@
-import React from 'react';
-import './components.css';
+import React from "react";
+import "./components.css";
 
 export default function HomeScreen() {
   return (
     <div className="Screen">
-      <header className="CorgiBox">
-        <img className="CorgiBox" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="CorgiBox" src={"../Assets/corgi_logo.svg"} />
     </div>
   );
 }
- 
