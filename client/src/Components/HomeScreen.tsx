e4382a08daa8fed435351b75cbdaca1d1fa2ab7d
Annette Lau <annette1@ualberta.ca>
2021-01-30 12:36:47 -0700

    home page bootiful

--- client/src/Components/HomeScreen.tsx
+++ client/src/Components/HomeScreen.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./components.css";
 import { ReactComponent as CorgiLogo } from "../Assets/corgi_logo.svg";
+import { ReactComponent as StartButton } from "../Assets/start_button.svg";
 
 export default function HomeScreen() {
   return (
-    <div className="Screen">
-      <CorgiLogo />
+    <div className="screen">
+      <div className="corgiBox">
+        <CorgiLogo />
+        <h1 className="bigText">Coronation</h1>
+      </div>
+      <div className="startBox">
+        <StartButton />
+      </div>
     </div>
   );
 }
