5fed90dbf37c00a96ddd8ac3bf647eed5b56aa4e
Annette Lau <annette1@ualberta.ca>
2021-01-30 19:15:35 -0700

    IDK WHAT IM DOING holy crap im sorryryyyy

--- client/src/Components/HomeScreen.tsx
+++ client/src/Components/HomeScreen.tsx
@@ -3,13 +3,14 @@ import "./components.css";
 import { ReactComponent as CorgiLogo } from "../Assets/corgi_logo.svg";
 import { ReactComponent as Team } from "../Assets/castle_butte.svg";
 import { ReactComponent as Start } from "../Assets/start_button.svg";
+import { ReactComponent as Title } from "../Assets/coronation.svg";
 
 export default function HomeScreen() {
   return (
     <div className="screen">
       <div className="titleBox">
         <CorgiLogo />
-        <h1 className="bigText">Coronation</h1>
+        <Title />
       </div>
       <a href="/hostjoin">
         <Start />
--- client/src/Components/WaitingScreen.tsx
+++ client/src/Components/WaitingScreen.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import { useState } from "react";
 import socket from "../socket";
 import "./components.css";
 import { ReactComponent as Start } from "../Assets/start_button.svg";
 import { useHistory } from "react-router-dom";
 import { Location } from "history";
 import { Game, Lobby } from "../../../types";
+import { ReactComponent as Eight } from "../Assets/8x8.svg";
+import { ReactComponent as Ten } from "../Assets/10x10.svg";
+import { ReactComponent as Twelve } from "../Assets/12x12.svg";
+import { ReactComponent as Fourteen } from "../Assets/14x14.svg";
+import { ReactComponent as Sixteen } from "../Assets/16x16.svg";
 
 export default function HomeScreen({
   location,
@@ -13,6 +19,7 @@ export default function HomeScreen({
 }) {
   const history = useHistory();
   const lobby = location.state;
+
   function startGame() {
     socket.emit("start", (game: Game) => {
       history.push({ pathname: "/game", state: game });
@@ -23,18 +30,41 @@ export default function HomeScreen({
       history.push({ pathname: "/game", state: game });
     });
   }
+
+  const [size, setSize] = useState();
+  const boardSizes = [
+    <div className="buttonBox">
+      <Eight />
+    </div>,
+    <div className="buttonBox">
+      <Ten />
+    </div>,
+    <div className="buttonBox">
+      <Twelve />
+    </div>,
+    <div className="buttonBox">
+      <Fourteen />
+    </div>,
+    <div className="buttonBox">
+      <Sixteen />
+    </div>,
+  ];
+
   return (
     <div className="screen">
       <h1 style={{ color: "#F4B670" }} className="mediumText">
         Game Code: {lobby.code}
       </h1>
       <div className="playerBox">
-        <h1 className="smallText">Host: Corgi White</h1>
-        <h1 style={{ color: "#F4B670" }} className="smallText">
-          Joined: Corgi Beige
-        </h1>
+        <h1 className="smallText">Host: White</h1>
+        <h1 className="smallText">Joined: Black</h1>
       </div>
       <h1 className="smallText">Board Size: {lobby.size}</h1>
+
+      {boardSizes.map((boardSize) => (
+        <li>{boardSize}</li>
+      ))}
+
       <div className="buttonBox" onClick={startGame}>
         <Start />
       </div>
